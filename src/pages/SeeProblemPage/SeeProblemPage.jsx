c362d188a4182c60b2f47d8a37bff1759f63dfc5
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2024-03-24 19:36:55 +0300

    Delete comments

--- src/pages/SeeProblemPage/SeeProblemPage.jsx
+++ src/pages/SeeProblemPage/SeeProblemPage.jsx
@@ -4,65 +4,11 @@ import "./example.css"
 import "./style.css"
 import "./utils.css"
 import {useNavigate, useParams} from "react-router-dom";
-// import getApiAddress from "../../../utils/api";
-// import {toast} from "react-toastify";
-
-// import "react-ace"
-// import "ace-builds/src-noconflict/mode-python";
-// import "ace-builds/src-noconflict/snippets/python";
-// import "ace-builds/src-noconflict/theme-tomorrow";
-// import "ace-builds/src-noconflict/ext-language_tools";
 import Markdown from "../../components/wraps/Markdown.jsx";
-
-// import apiAxios from "../../../apiAxios";
-
-
-// import defaultFile from 'src/build.json'
-
 const SeeProblemPage = ({defaultInfo}) => {
-    const [info, setInfo] = useState({langs: {}, examples: []});
 
     const {id} = useParams();
-
-    // const navigate = useNavigate();
-    //
-    // const [lang, setLang] = useState(1);
-    //
-    // let editorCode;
-
-    // useEffect(() => {
-    //     apiAxios.get(getApiAddress() + `/api/teacher/problems/${id}`).then(
-    //         (r) => {
-    //             if (! r.data.langs) r.data.langs = {}
-    //             if (! r.data.examples) r.data.examples = []
-    //
-    //             console.log(r.data)
-    //
-    //             setInfo(r.data)
-    //         }
-    //     )
-    // }, []);
-
-    useEffect(() => {
-
-
-        setInfo(defaultInfo)
-
-    }, []);
-
-    // useEffect(() => {
-    //     if (info.langs.length > 0) {
-    //         setLang(info.langs[0])
-    //     }
-    //     console.log(info.langs)
-    // }, [info]);
-    //
-
-    console.log(info)
-
     return <>
-        {/*<PageTitle title={`Задача id=` + id }/>*/}
-
         <div className="p-3 mb-4 bg-danger theme-text-light rounded-3 text-center">
             Просмотр в режиме учителя
         </div>
@@ -71,7 +17,7 @@ const SeeProblemPage = ({defaultInfo}) => {
             <div className="col-md-6">
                 <div className="h-60 p-3 theme-bg-light rounded-3">
                     <h2 style={{"color": "#6c757d;"}}>Задача id={id}</h2>
-                    <h3>{info.name}</h3>
+                    <h3>{defaultInfo.name}</h3>
                 </div>
             </div>
             <div className="col-md-6">
@@ -91,11 +37,11 @@ const SeeProblemPage = ({defaultInfo}) => {
             <div className="container-fluid py-5">
                 <div className="col-md-12 fs-4">
                     <h4>Задача</h4>
-                    <Markdown text={info.description}/>
+                    <Markdown text={defaultInfo.description}/>
                     <h4>Входные данные</h4>
-                    <Markdown text={info.in_data || info.in}/>
+                    <Markdown text={defaultInfo.in_data || defaultInfo.in}/>
                     <h4>Выходные данные</h4>
-                    <Markdown text={info.out_data || info.out}/>
+                    <Markdown text={defaultInfo.out_data || defaultInfo.out}/>
                 </div>
 
 
@@ -106,7 +52,7 @@ const SeeProblemPage = ({defaultInfo}) => {
             <h2>Примеры</h2>
 
             {
-                info.examples.map((data, index) => {
+                defaultInfo.examples.map((data, index) => {
                     const [input_data, output_data] = data
 
                     const example_id = `example_${index}`
@@ -114,7 +60,6 @@ const SeeProblemPage = ({defaultInfo}) => {
                     const copyFunction = () => {
                         const copyText = document.getElementById(example_id);
 
-                        // toast.success('Успешно скопировано');
 
                         console.log(copyText)
                         navigator.clipboard.writeText(copyText.innerText);
